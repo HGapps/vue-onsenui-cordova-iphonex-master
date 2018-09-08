f37c60f58c9a1b4bf91c4423c877a1a5e04ed9f4
Hasan Gad Allah <hasan.gadallah@gmail.com>
2018-09-08 13:31:48 +0300

    1st

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <v-ons-page>
+      <v-ons-navigator
+        :page-stack="pageStack"
+        :animation="animation"
+        @push-page="pageStack.push($event)"
+        @pop-page="pageStack.pop()"
+        @change-animation="animation = $event"
+      />
+    </v-ons-page>
+  </div>
+</template>
+
+<script>
+import TabsPage from '@/components/TabsPage';
+import CoverPage from '@/components/CoverPage';
+
+export default {
+  data() {
+    return {
+      pageStack: [
+        TabsPage,
+        CoverPage,
+      ],
+      animation: 'fade',
+    };
+  },
+  beforeMount() {
+    const html = document.documentElement;
+    if (this.$ons.platform.isIPhoneX()) {
+      html.setAttribute('onsflag-iphonex-portrait', '');
+      html.setAttribute('onsflag-iphonex-landscape', '');
+    }
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TabsPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-ons-page @show="$emit('change-animation', 'lift')">
+    <v-ons-tabbar
+      position="bottom"
+      :tabs="tabs"
+      animation="none"
+      @push-page="$emit('push-page', $event)"
+    />
+  </v-ons-page>
+</template>
+
+<script>
+import NotesPage from './tabs/NotesPage';
+
+export default {
+  data() {
+    return {
+      tabs: [
+        {
+          icon: 'fa-book',
+          label: 'Notes',
+          page: NotesPage,
+        },
+        {
+          icon: 'fa-tag',
+          label: 'Tags',
+          page: {
+            template: '<v-ons-page>Dummy</v-ons-page>',
+          },
+        },
+        {
+          icon: 'md-search',
+          label: 'Search',
+          page: {
+            template: '<v-ons-page>Dummy</v-ons-page>',
+          },
+        },
+        {
+          icon: 'fa-bar-chart',
+          label: 'Stats',
+          page: {
+            template: '<v-ons-page>Dummy</v-ons-page>',
+          },
+        },
+        {
+          icon: 'fa-ellipsis-h',
+          label: 'More',
+          page: {
+            template: '<v-ons-page>Dummy</v-ons-page>',
+          },
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
